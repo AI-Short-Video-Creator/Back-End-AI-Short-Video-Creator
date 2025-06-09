bc69d5fdb4d9d5478a6208c8bf0880e8f59dc931
22120278 <nvhphuc364@gmail.com>
2025-06-09 19:03:52 +0700

    add JWT authentication to script route

--- app/script/route.py
+++ app/script/route.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 from app.script.service import ScriptService
 from app.script.dto import ScriptGenerateDTO, ScriptGenerateResponseDTO
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 class ScriptController:
     def __init__(self):
@@ -12,11 +13,14 @@ class ScriptController:
     def _register_routes(self):
         self.script_bp.add_url_rule('/', view_func=self.script, methods=['POST'])
 
+    @jwt_required()
     def script(self):
         """Script route handler."""
         try:
             data = request.json
             dto = ScriptGenerateDTO(**data)
+            user_id = get_jwt_identity()  # Get the user ID from JWT token
+            print(f"User ID: {user_id}")  # Debugging line to check user ID
 
             script = self.service.generate_script(dto)
             return jsonify({"data": script}), 200
